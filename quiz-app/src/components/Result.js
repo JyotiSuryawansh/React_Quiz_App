c993f536273024e6c51489e5c62636d61826385a
JyotiSuryawansh <sjyoti199810@gmail.com>
2022-04-22 14:51:14 +0530

    update

--- quiz-app/src/components/Result.js
+++ quiz-app/src/components/Result.js
@@ -14,7 +14,7 @@ const Result = (props) =>{
     const {setQuestions} =a;
  
     const handleSubmit = () =>{
-        setQuestions();
+        //setQuestions();
     }
 
     return(
--- quiz-app/src/components/quiz.js
+++ quiz-app/src/components/quiz.js
@@ -1,4 +1,4 @@
-import DataBase from "./Data";
+
 import React,{useState,useEffect,useContext} from "react";
 import "./Quiz.css"
 import { AppContext } from "./Context";
